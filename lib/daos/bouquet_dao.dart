0a70a609d0edab83bf222db4ccb44d681917d81b
Mohamed El Béchir Traoré <mohamedelbechir1@gmail.com>
2023-10-16 23:51:17 +0000

    fix sql queries

--- lib/daos/bouquet_dao.dart
+++ lib/daos/bouquet_dao.dart
@@ -1,20 +1,9 @@
 import 'package:drift/drift.dart';
 import 'package:jamanacanal/models/bouquet.dart';
+import 'package:jamanacanal/models/bouquet_detail.dart';
 import 'package:jamanacanal/models/database.dart';
 
 part 'bouquet_dao.g.dart';
-
-class BouquetDetail {
-  final String id;
-  final String name;
-  final int nombreOfActiveSubcription;
-
-  BouquetDetail({
-    required this.id,
-    required this.name,
-    required this.nombreOfActiveSubcription,
-  });
-}
 
 @DriftAccessor(tables: [Bouquets])
 class BouquetsDao extends DatabaseAccessor<AppDatabase>
@@ -30,7 +19,7 @@ class BouquetsDao extends DatabaseAccessor<AppDatabase>
             obsolete, 
             create_at,
             update_at, 
-            (SELECT COUNT(*) FROM subscriptions s WHERE start_date >= DATE('now') AND end_date >= DATE('now') AND b.id = b.bouquet_id) AS number_of_active_subscription
+            (SELECT COUNT(*) FROM subscriptions s WHERE s.start_date <=  strftime('%s', 'now') AND s.end_date >= strftime('%s', 'now') AND b.id = s.bouquet_id ) AS number_of_active_subscription
       FROM bouquets b;
 
       """,
@@ -38,6 +27,7 @@ class BouquetsDao extends DatabaseAccessor<AppDatabase>
       return BouquetDetail(
           id: row.read("id"),
           name: row.read("name"),
+          updateAt: row.read("update_at"),
           nombreOfActiveSubcription:
               row.read<int>("number_of_active_subscription"));
     }).get();
--- lib/daos/customer_dao.dart
+++ lib/daos/customer_dao.dart
@@ -21,27 +21,34 @@ class CustomersDao extends DatabaseAccessor<AppDatabase>
   Future<List<CustomerDetail>> allCustomerDetails() {
     return customSelect(
       """
-      SELECT id, 
-            first_name, 
-            last_name, 
-            phone_number, 
-            number_customer, 
-            (SELECT COUNT(*) FROM decoders d WHERE d.customer_id = c.id ) AS number_of_decoder,
-            (SELECT COUNT(*) FROM subscriptions s WHERE start_date >= DATE('now') AND end_date >= DATE('now')) AS number_of_active_subscription
-      FROM customers c;
+      SELECT c.id, 
+             first_name, 
+             last_name, 
+             phone_number, 
+             number_customer, 
+             count(d.id) as number_of_decoder,
+             (SELECT COUNT(*) 
+              FROM subscriptions s 
+              WHERE  s.decoder_id = d.id 
+                    AND start_date <= strftime('%s', 'now') 
+                    AND end_date >= strftime('%s', 'now')) 
+                    AS number_of_active_subscription
+
+      FROM customers c
+      LEFT JOIN decoders d on d.customer_id = c.id
+      GROUP BY c.id;
 
       """,
       // used for the stream: the stream will update when either table changes
     ).map((QueryRow row) {
       return CustomerDetail(
         id: row.read<int>('id'),
-        custumerNumber: row.read<String>("number_customer"),
-        firstName: row.read<String>("first_name"),
-        lastName: row.read<String>("last_name"),
-        phoneNumber: row.read<String>("phone_number"),
-        numberOfDecoder: row.read<int>("number_of_decoder"),
-        numberOfActiveSubscription:
-            row.read<int>("number_of_active_subscription"),
+        custumerNumber: row.read("number_customer"),
+        firstName: row.read("first_name"),
+        lastName: row.read("last_name"),
+        phoneNumber: row.read("phone_number"),
+        numberOfDecoder: row.read("number_of_decoder"),
+        numberOfActiveSubscription: row.read("number_of_active_subscription"),
       );
     }).get();
   }
