9229b84b5ebc0933e36663b45ba3033f0f752200
Mohamed El Béchir Traoré <mohamedelbechir1@gmail.com>
2023-10-21 17:23:09 +0000

    add serialisation feature

--- lib/models/subscription_detail.dart
+++ lib/models/subscription_detail.dart
@@ -1,3 +1,6 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
 import 'package:equatable/equatable.dart';
 
 class SubscriptionDetail extends Equatable {
@@ -35,4 +38,37 @@ class SubscriptionDetail extends Equatable {
         paid,
         phoneNumber,
       ];
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'startDate': startDate.millisecondsSinceEpoch,
+      'endDate': endDate.millisecondsSinceEpoch,
+      'bouquetName': bouquetName,
+      'decoderNumber': decoderNumber,
+      'phoneNumber': phoneNumber,
+      'customerFullName': customerFullName,
+      'customerId': customerId,
+      'paid': paid,
+    };
+  }
+
+  factory SubscriptionDetail.fromMap(Map<String, dynamic> map) {
+    return SubscriptionDetail(
+      id: map['id'] as int,
+      startDate: DateTime.fromMillisecondsSinceEpoch(map['startDate'] as int),
+      endDate: DateTime.fromMillisecondsSinceEpoch(map['endDate'] as int),
+      bouquetName: map['bouquetName'] as String,
+      decoderNumber: map['decoderNumber'] as String,
+      phoneNumber: map['phoneNumber'] as String,
+      customerFullName: map['customerFullName'] as String,
+      customerId: map['customerId'] as int,
+      paid: map['paid'] as bool,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory SubscriptionDetail.fromJson(String source) =>
+      SubscriptionDetail.fromMap(json.decode(source) as Map<String, dynamic>);
 }
