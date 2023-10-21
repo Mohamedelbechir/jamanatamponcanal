a076c10a59727c514670431b940eb4cfc15e8fc4
Mohamed El Béchir Traoré <mohamedelbechir1@gmail.com>
2023-10-21 17:22:13 +0000

    add remove feature for customer

--- lib/pages/customer/widgets/customer_tile.dart
+++ lib/pages/customer/widgets/customer_tile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:jamanacanal/cubit/customer/customer_cubit.dart';
 import 'package:jamanacanal/cubit/notification/notification_cubit.dart';
 import 'package:jamanacanal/models/customer_detail.dart';
@@ -57,48 +58,74 @@ class _CustomerTileState extends State<CustomerTile> {
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: showCustomerFormDialog,
-      child: Container(
-        padding: const EdgeInsets.all(8.0),
-        decoration: BoxDecoration(
-          color: Colors.grey[50],
-          borderRadius: BorderRadius.circular(5),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            CustomerFullName(
-              customerFullName:
-                  "${widget.customer.firstName} ${widget.customer.lastName}",
-            ),
-            Wrap(
-              children: [
-                PhoneNumberWidget(phoneNumber: widget.customer.phoneNumber),
-                const SizedBox(width: 5),
-                CustomerNumberWidget(
-                  numberCustomer:
-                      widget.customer.custumerNumber.split("|").join(" | "),
-                ),
-              ],
-            ),
-            Wrap(
-              children: [
-                BoldTag(
-                  color: Colors.blue,
-                  text:
-                      "${widget.customer.numberOfActiveSubscription} abonnement actif (s)",
-                ),
-                const SizedBox(width: 5),
-                BoldTag(
-                  color: Colors.blue,
-                  text: "${widget.customer.numberOfDecoder} décodeur (s)",
-                ),
-              ],
-            ),
-          ],
+    return Slidable(
+      endActionPane: ActionPane(
+        motion: const ScrollMotion(),
+        children: [
+          SlidableAction(
+            flex: 1,
+            onPressed: (_) {
+              context.read<CustomerCubit>().remove(widget.customer.id);
+            },
+            backgroundColor: Colors.red,
+            foregroundColor: Colors.white,
+            icon: Icons.delete,
+            label: 'Supprimer',
+          ),
+        ],
+      ),
+      child: InkWell(
+        onTap: showCustomerFormDialog,
+        child: Container(
+          padding: const EdgeInsets.all(8.0),
+          decoration: BoxDecoration(
+            color: Colors.grey[50],
+            borderRadius: BorderRadius.circular(5),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CustomerFullName(
+                customerFullName:
+                    "${widget.customer.firstName} ${widget.customer.lastName}",
+              ),
+              Wrap(
+                children: [
+                  if (widget.customer.hasPhoneNumber)
+                    PhoneNumberWidget(
+                      phoneNumber: widget.customer.phoneNumber!,
+                    ),
+                  const SizedBox(width: 5),
+                  if (_canDisplayCustomerNumber)
+                    CustomerNumberWidget(
+                      numberCustomer: widget.customer.custumerNumber!
+                          .split("|")
+                          .join(" | "),
+                    ),
+                ],
+              ),
+              Wrap(
+                children: [
+                  BoldTag(
+                    color: Colors.blue,
+                    text:
+                        "${widget.customer.numberOfActiveSubscription} abonnement actif (s)",
+                  ),
+                  const SizedBox(width: 5),
+                  BoldTag(
+                    color: Colors.blue,
+                    text: "${widget.customer.numberOfDecoder} décodeur (s)",
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
+
+  bool get _canDisplayCustomerNumber =>
+      widget.customer.custumerNumber != null &&
+      widget.customer.custumerNumber!.isNotEmpty;
 }
